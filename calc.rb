edb7036d7aa5373c92afddbd4c112731e89cd5f0
montocore <39366155+montocore@users.noreply.github.com>
2018-05-17 00:36:41 -0700

    Add files via upload

---
+++ calc.rb
@@ -0,0 +1,36 @@
+puts "Welcome to Zach's Calculator"
+
+print "Enter a symbol:"
+
+symbol = gets.to_c
+
+
+print "Enter a Value:"
+
+first_num = gets.to_f
+
+print "Enter one more:"
+
+second_num = gets.to_f
+
+if symbol == " / "
+	puts first_num.to_s + " / " + second_num.to_s + " = " + (first_num / second_num).to_s
+end
+	
+	if symbol == " * "
+		puts first_num.to_s + " * " + second_num.to_s + " = " + (first_num * second_num).to_s
+end
+		
+		if symbol == " + "
+		puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
+end	
+			
+			if symbol == " - "
+puts first_num.to_s + " - " + second_num.to_s + " = " + (first_num - second_num).to_s
+end
+
+
+
+
+
+
